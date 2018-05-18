fd9f401e1fcc7faf5b9249ddd47bd60e0263731e
tvxx <39387216+tvxx@users.noreply.github.com>
2018-05-18 01:25:29 +0000

    boilerplate

---
+++ app/api/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from .models import ApiService
+# Register your models here.
---
+++ app/api/models.py
@@ -0,0 +1,26 @@
+#author:tonyv
+#lic:MIT
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class ApiService(models.Model):
+  param1 = models
+  param2 = models
+  param2 = models
+
+
+  def validKey(self):
+    self.key = 1
+
+  def translate(self,p1,p2):
+    self.p1 = p1
+    self.p2 = p2
+
+  def handlePost(self,p1,p2):
+    return true
+
+  def __str__(self):
+    return self
